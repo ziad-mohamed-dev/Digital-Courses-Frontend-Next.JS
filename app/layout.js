e0ebc4885baecde6b9db50c5683f41424e2bb197
Hunter-Smsm <ziad35199@gmail.com>
2024-07-18 12:44:01 +0300

    update

--- app/layout.js
+++ app/layout.js
@@ -17,8 +17,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <>
-      <Analytics />
-      <SpeedInsights />
+      {/* <Analytics /> */}
+      {/* <SpeedInsights /> */}
       <ClerkProvider>
         <CartContextProvider>
           <html lang="en" className="dark">
